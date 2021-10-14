a8b199ac6ea30d0c6e4df4a802fd9ad75c2a081d
manxv7 <manav12ahuja@gmail.com>
2021-10-14 20:47:57 +0530

    Fixing Toggling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,43 +1,40 @@
 //Checking off the completed todo
-const p=document.querySelector("ul");
+const p = document.querySelector("ul");
 // hello
-    p.addEventListener("click",(e)=>{
-        const target=e.target;
-        if(target.matches("li"))
-        {
-            target.classList.toggle("grey");
-        }  
-    });
+p.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.matches("li")) {
+        target.classList.toggle("grey");
+    }
+});
 
 
 //removing todo
 
-p.addEventListener("click",(e)=>{
-    const target=e.target;  
-    if(target.matches("i"))
-    {
-        const parent=target.parentElement.parentElement;
+p.addEventListener("click", (e) => {
+    const target = e.target;
+    if (target.matches("i")) {
+        const parent = target.parentElement.parentElement;
         parent.classList.add("removed");
         e.stopPropagation();
-        parent.addEventListener("transitionend",()=>{
+        parent.addEventListener("transitionend", () => {
             parent.remove();
         })
     }
-   
+
 })
 
 //adding todos
 
 
-const ul=document.querySelector("ul");
-const input=document.querySelector("input");
-input.addEventListener("keypress",(event)=>{
-    if(event.key==="Enter")
-    {
-        let todoText=input.value;
-        const newLi=document.createElement("li");
-        newLi.innerHTML=`<span><i class="fa fa-trash"></i></span> ${todoText}`;
-        input.value="";
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+input.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        let todoText = input.value;
+        const newLi = document.createElement("li");
+        newLi.innerHTML = `<span><i class="fa fa-trash"></i></span> ${todoText}`;
+        input.value = "";
         ul.append(newLi);
 
     }
@@ -46,12 +43,19 @@ input.addEventListener("keypress",(event)=>{
 
 //toggling the input
 
-const i=document.querySelector("i");
+const i = document.querySelector("i");
 
-i.addEventListener("click",()=>{
-// console.log(input.style.display);
-    input.style.opacity=0;
-    window.setTimeout(()=>{
-        input.style.display="none";
-    },300)
+i.addEventListener("click", () => {
+    // console.log(input.style.display);
+    if (input.style.display === "block") {
+        input.style.opacity = 0;
+        window.setTimeout(() => {
+            input.style.display = "none";
+        }, 30)
+    } else {
+        input.style.opacity = 1;
+        window.setTimeout(() => {
+            input.style.display = "block";
+        }, 30)
+    }
 })
